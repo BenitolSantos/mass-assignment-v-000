7a7d5a7f52878875134a0c69a8eccefe1aace772
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-22 15:25:47 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,7 @@ class Person
   def initialize(personal_attributes) #it accepts a hash and every value becomes an attribute
     personal_attributes.each {|key,value| self.send(("#{key}="), value)}
     #so we need to each what the initialize is accepting
+    #ALSO REMEMBER TO CLOSE EVERY PARENTHESE AND BRACKETS
     #quotes works better here when interpolating
   end
 end
