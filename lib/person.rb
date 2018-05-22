d43eea8a3f9bb9799308261a4ae9c9425c0dbb53
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-22 14:57:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   #your code here
+  def initialize(personal_attributes) #it accepts a hash and every value becomes an attribute
+    personal_attributes.each
+  end
 end
