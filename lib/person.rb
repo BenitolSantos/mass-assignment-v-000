0f5fd05b8a7d75aadf2c92eb9882e5829b2108b1
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-22 15:22:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   #your code here
   def initialize(personal_attributes) #it accepts a hash and every value becomes an attribute
-    #personal_attributes.each {|key,value| self.send("#{key}="), value)}
+    personal_attributes.each {|key,value| self.send("#{key}="), value)}
     #quotes works better here when interpolating
   end
 end
